7b94748f14c5e0115288921ce7cf74ddf7b6ed1a
Petter Holmström <petter@vaadin.com>
2024-01-26 10:45:35 +0200

    Navigation items

--- unified-tutorial-flow/src/main/java/com/example/application/ui/MainLayout.java
+++ unified-tutorial-flow/src/main/java/com/example/application/ui/MainLayout.java
@@ -1,11 +1,18 @@
 package com.example.application.ui;
 
+import com.example.application.ui.view.customer.CustomerNavigation;
+import com.example.application.ui.view.start.StartView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.sidenav.SideNav;
+import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.theme.lumo.LumoUtility.*;
 
@@ -13,8 +20,10 @@ import java.util.Optional;
 
 public class MainLayout extends AppLayout {
 
-    private H1 viewTitle;
-    private Div viewNavbarContent;
+    // TODO Is it a good practice to create the fields like this, and configure them in a private method,
+    //  or should they be configured in the constructor?
+    private final H1 viewTitle = new H1();
+    private final Div viewNavbarContent = new Div();
 
     public MainLayout() {
         setPrimarySection(Section.DRAWER);
@@ -28,10 +37,8 @@ public class MainLayout extends AppLayout {
         toggle.setAriaLabel("Menu toggle");
         toggle.setTooltipText("Menu toggle");
 
-        viewTitle = new H1();
         viewTitle.addClassNames(FontSize.LARGE);
 
-        viewNavbarContent = new Div();
         viewNavbarContent.addClassNames(Margin.Start.AUTO);
 
         var header = new Header(toggle, viewTitle, viewNavbarContent);
@@ -47,27 +54,22 @@ public class MainLayout extends AppLayout {
 
         var scroller = new Scroller(createNavigation());
 
-        addToDrawer(appName, scroller, createFooter());
+        addToDrawer(appName, scroller);
     }
 
     private SideNav createNavigation() {
-        SideNav nav = new SideNav();
-
-        /*nav.addItem(new SideNavItem("Hello World", HelloWorldView.class, LineAwesomeIcon.GLOBE_SOLID.create()));
-        nav.addItem(new SideNavItem("About", AboutView.class, LineAwesomeIcon.FILE.create()));
-        nav.addItem(new SideNavItem("Master-Detail", MasterDetailView.class, LineAwesomeIcon.COLUMNS_SOLID.create()));*/
-
+        var nav = new SideNav();
+        // TODO Here we refer to the StartView directly and to the CustomerView indirectly. Is this inconsistent?
+        nav.addItem(new SideNavItem("Start", StartView.class, VaadinIcon.HOME.create()));
+        nav.addItem(new SideNavItem("Customers", CustomerNavigation.allCustomersView(), VaadinIcon.BRIEFCASE.create()));
         return nav;
-    }
-
-    private Footer createFooter() {
-        return new Footer();
     }
 
     @Override
     protected void afterNavigation() {
         super.afterNavigation();
         viewTitle.setText(getCurrentPageTitle());
+        // TODO Is it a good idea to allow views to add components to the navbar?
         viewNavbarContent.removeAll();
         getCurrentNavbarContent().ifPresent(viewNavbarContent::add);
     }
---
+++ unified-tutorial-flow/src/main/java/com/example/application/ui/view/start/StartView.java
@@ -0,0 +1,11 @@
+package com.example.application.ui.view.start;
+
+import com.example.application.ui.MainLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "", layout = MainLayout.class)
+public class StartView extends VerticalLayout {
+
+    // TODO What should we put here?
+}
