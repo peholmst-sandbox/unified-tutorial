1dd2d9ce7aeb024c0c2f8e1ae6550a80f3ec88d6
Petter Holmström <petter@vaadin.com>
2024-02-06 16:30:01 +0200

    Replace states with signals in LobbyView

--- unified-chat-tutorial-hilla/frontend/index.ts
+++ unified-chat-tutorial-hilla/frontend/index.ts
@@ -1,5 +1,9 @@
-import { createElement } from 'react';
-import { createRoot } from 'react-dom/client';
+import {createElement} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App.js';
+import {installAutoSignalTracking} from "@preact/signals-react/runtime";
+
+// Without this, we can't use signals in our components
+installAutoSignalTracking();
 
 createRoot(document.getElementById('outlet')!).render(createElement(App));
--- unified-chat-tutorial-hilla/frontend/views/lobby/LobbyView.tsx
+++ unified-chat-tutorial-hilla/frontend/views/lobby/LobbyView.tsx
@@ -3,7 +3,6 @@ import {Chat} from "Frontend/generated/endpoints";
 import {Grid} from "@hilla/react-components/Grid";
 import {GridColumn} from "@hilla/react-components/GridColumn";
 import {VerticalLayout} from "@hilla/react-components/VerticalLayout";
-import {useEffect, useState} from "react";
 import {useAuth} from "Frontend/util/auth";
 import {HorizontalLayout} from "@hilla/react-components/HorizontalLayout";
 import {TextField} from "@hilla/react-components/TextField";
@@ -11,55 +10,51 @@ import {Button} from "@hilla/react-components/Button";
 import {Link} from "react-router-dom";
 
 import {pageTitle} from "Frontend/views/MainLayout";
+import {signal, useSignal} from "@preact/signals-react";
+import {useEffect} from "react";
 
-type AddRoomProps = {
-    onRoomAddedCallback?: () => void;
+pageTitle.value = "Lobby";
+
+const rooms = signal<ChatRoom[]>([]);
+const refreshRooms = async () => {
+    rooms.value = await Chat.rooms();
 }
 
-function AddRoom(props: AddRoomProps) {
-    const [name, setName] = useState('');
-    const [buttonDisabled, setButtonDisabled] = useState(false);
+function AddRoomComponent() {
+    const name = useSignal('');
+    const buttonDisabled = useSignal(false);
 
-    const addRoom = () => {
-        if (name !== '') {
-            setButtonDisabled(true);
-            Chat.createRoom(name).then(() => {
-                if (props.onRoomAddedCallback) {
-                    props.onRoomAddedCallback();
-                }
-                setName('');
-                setButtonDisabled(false);
-            });
+    const addRoom = async () => {
+        if (name.value !== '') {
+            buttonDisabled.value = true;
+            try {
+                await Chat.createRoom(name.value);
+                name.value = '';
+                await refreshRooms();
+            } finally {
+                buttonDisabled.value = false;
+            }
         }
     };
 
     // TODO Assign ENTER as shortcut key to button
-
     return <HorizontalLayout theme={"spacing"} className={"w-full"}>
-        <TextField placeholder={"New chat room name"} className={"flex-grow"} value={name}
-                   onChange={e => setName(e.target.value)}/>
-        <Button theme={"primary"} onClick={addRoom} disabled={buttonDisabled}>Add room</Button>
+        <TextField placeholder={"New chat room name"} className={"flex-grow"} value={name.value}
+                   onChange={e => name.value = e.target.value}/>
+        <Button theme={"primary"} onClick={addRoom} disabled={buttonDisabled.value}>Add room</Button>
     </HorizontalLayout>
 }
 
 export default function LobbyView() {
     const {hasAccess} = useAuth();
-    pageTitle.value = "Lobby";
-
-    const [rooms, setRooms] = useState<ChatRoom[]>([]);
-
-    const refreshRooms = () => {
-        Chat.rooms().then(result => setRooms(result));
-    }
-
-    useEffect(() => {
-        refreshRooms();
-    }, []);
-
     const isAdmin = hasAccess({rolesAllowed: ["ROLE_ADMIN"]});
 
+    useEffect(() => {
+        (async () => await refreshRooms())();
+    }, []);
+
     return <VerticalLayout theme={"spacing padding"} className={"h-full w-full"}>
-        <Grid className={"h-full w-full"} items={rooms}>
+        <Grid className={"h-full w-full"} items={rooms.value}>
             <GridColumn header={"Room Name"}>
                 {({item}) => {
                     return <Link to={"chatroom/" + item.id}>{item.name}</Link>
@@ -71,6 +66,6 @@ export default function LobbyView() {
                 }}
             </GridColumn>
         </Grid>
-        {isAdmin && <AddRoom onRoomAddedCallback={refreshRooms}/>}
+        {isAdmin && <AddRoomComponent/>}
     </VerticalLayout>;
 }
