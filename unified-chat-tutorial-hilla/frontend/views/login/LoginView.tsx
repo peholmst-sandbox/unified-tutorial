44a686009e3166b1ff77e09b18392daebb98e462
Petter Holmström <petter@vaadin.com>
2024-02-06 11:06:33 +0200

    Replace state with signal in LoginView

--- unified-chat-tutorial-hilla/frontend/views/login/LoginView.tsx
+++ unified-chat-tutorial-hilla/frontend/views/login/LoginView.tsx
@@ -1,11 +1,11 @@
 import {useAuth} from 'Frontend/util/auth.js';
-import {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {LoginForm} from "@hilla/react-components/LoginForm";
+import {signal} from "@preact/signals-react";
 
 export default function LoginView() {
     const {login} = useAuth();
-    const [hasError, setError] = useState<boolean>();
+    const hasError = signal(false);
     const navigate = useNavigate();
     document.title = "Login";
 
@@ -13,10 +13,10 @@ export default function LoginView() {
         <div className={"flex flex-col h-full w-full items-center justify-center gap-m"}>
             <h1>Vaadin Chat</h1>
             <div>You can log in as 'alice', 'bob' or 'admin'. The password for all of them is 'password'.</div>
-            <LoginForm error={hasError} onLogin={async ({detail: {username, password}}) => {
+            <LoginForm error={hasError.value} onLogin={async ({detail: {username, password}}) => {
                 const {defaultUrl, error, redirectUrl} = await login(username, password);
                 if (error) {
-                    setError(true);
+                    hasError.value = true;
                 } else {
                     const url = redirectUrl ?? defaultUrl ?? '/';
                     const path = new URL(url, document.baseURI).pathname;
