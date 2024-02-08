30260ed9c544076969c523bb2b43a05de4cf54f6
Petter Holmström <petter@vaadin.com>
2024-02-08 17:33:54 +0200

    Can't use domain primitives at this stage

--- unified-chat-tutorial-hilla/src/main/java/com/example/application/chat/Channel.java
+++ unified-chat-tutorial-hilla/src/main/java/com/example/application/chat/Channel.java
@@ -3,6 +3,7 @@ package com.example.application.chat;
 import jakarta.annotation.Nullable;
 
 public record Channel(
+        // TODO This should be a domain primitive, but it's currently not supported https://github.com/vaadin/hilla/issues/2055
         String id,
         String name,
         @Nullable Message lastMessage
